150c183872fdd3eafd2c73a28430f6fbdfebc360
Anas Mohamed <amohamed2479@gmail.com>
2022-08-23 21:15:06 -0500

    Create featured projects section

---
+++ src/projects.js
@@ -0,0 +1,46 @@
+
+export default [
+    {
+        title: 'Form Chimp',
+        description: 'A platform that allows you to build web forms without worrying about writing server code.',
+        url:'https://anasweb.notion.site/Form-Chimp-A-Project-By-Anas-Mohamed-3b68631525774aba97786a219f0005ea',
+        type:['Brainchild', 'Featured'],
+        logo:`/form-chimp.svg`
+    },
+    {
+        title: `End-Qabyaalad`,
+        description:`Creating social awareness and education campaigns about the disadvantages and backwardness of the patrimonial system of Clannism (Qabyaalad).`,
+        url:`https://anasweb.notion.site/End-Qabyaalad-38929ef4860148878121d18f1c2aaec8`,
+        type: [`Non-Profit`, 'Featured'],
+        logo:`/end-qabyaalad.svg`
+    },
+    {
+        title: `Asker JS`,
+        description:`An interactive elegant web form powerd by JS.`,
+        url:`https://anasweb.notion.site/Asker-JS-A-Project-By-Anas-Mohamed-fae95c4994214d898a0f3ae51483c7a2`,
+        type:[`Brainchild`],
+        logo:`/asker-js.svg`
+    },
+    {
+        title: `MeProfile.site`,
+        description:`A platform for creating simple, yet elegant personal/profile sites.`,
+        url:`https://anasweb.notion.site/meprofile-site-A-Project-By-Anas-Mohamed-2261881ffda94e44bc67c831afd5c777`,
+        type:[`Brainchild`, 'Featured '],
+        logo:`/me-profile.svg`
+    },
+    {
+        title: "Shell Ninja",
+        description:"A web-based interactive tutorial that is designed to teach beginners the basics of using shell programs.",
+        url:"https://anasweb.notion.site/Shell-Ninja-1647c30a30ec44d2848738613327a5c1",
+        type:['Brainchild', 'Featured'],
+        logo:`/shell-ninja.png`
+    },
+    {
+        title: `Bash Mirror`,
+        description:`A toy JS library for creating Bash or Shell-like experiences on your web apps.`,
+        url:`https://anasweb.notion.site/Bash-Mirror-A-Project-By-Anas-Mohamed-37001173958b4040b4879de34b7a9227`,
+        type:[`Brainchild`],
+        logo:`/bash-mirror.png`
+    }
+    
+]
